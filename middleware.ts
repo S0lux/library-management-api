b0c0b0cfd3aff77c71da40937ac050b125be099c
S0lux <loctranphuoc456@protonmail.com>
2023-12-02 21:23:11 +0700

    Vercel debugging #3

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
 import { verifyToken } from './utils/jwtHandler'
  
-export async function middleware(request: NextRequest) {
+export async function middleware(request: Request) {
 
     const jwtToken = request.headers.get("Authorization")?.split(" ")[1]
-    console.log(request.headers.get("Authorization"))
+    console.log(request)
     let payload
 
     // Authentication with JWT
