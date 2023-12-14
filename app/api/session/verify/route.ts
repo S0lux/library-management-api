7981b7326d31a2d8547eb1c78442d39ba111932d
S0lux <loctranphuoc456@protonmail.com>
2023-12-14 17:37:42 +0700

    Auth endpoints now return Employee objects instead of just usernames

--- app/api/session/verify/route.ts
+++ app/api/session/verify/route.ts
@@ -1,14 +1,27 @@
-import { verifyToken } from '@/utils/jwtHandler'
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
+const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
 
     const token = req.headers.get("token")
     const username = req.headers.get("username")
 
+    const account = await prisma.account.findFirstOrThrow({
+        where: {
+            Username: username!
+        }
+    })
+
+    const employee = await prisma.employee.findFirstOrThrow({
+        where: {
+            EmployeeID: account.OwnerID
+        }
+    })
+
     const response = {
-        username: username,
+        ...employee,
         token: token,
     }
     
