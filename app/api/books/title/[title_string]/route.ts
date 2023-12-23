dd4be8e23e293ab640363d59df572f90b34d83fd
S0lux <loctranphuoc456@protonmail.com>
2023-12-23 19:39:39 +0700

    API route for searching books

---
+++ app/api/books/title/[title_string]/route.ts
@@ -0,0 +1,40 @@
+import { book } from "@/types/book";
+import { SearchResponse } from "@/types/searchRes";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, { params }: { params: { title_string: string } }) {
+    try {
+        const title = params.title_string.replaceAll(" ", "+");
+        let response: book[] = []
+
+        const res = await fetch("https://openlibrary.org/search.json?"
+            + `title=${title}&`
+            + `fields=key,title,author_name,editions,editions.isbn,editions.publish_date&`
+            + `limit=50`)
+
+        const searches: SearchResponse = await res.json();
+
+        searches.docs.forEach(doc => {
+            doc.editions.docs.forEach(book => {
+                if (book.isbn?.[0]) {
+                    response.push({
+                        Author: doc.author_name?.[0] || "Not found",
+                        Title: doc.title,
+                        ISBN13: book.isbn[0],
+                        PublishDate: new Date(book.publish_date[0]).toISOString()
+                    })
+                }
+            })
+        });
+
+        if (response.length < 1) {
+            return NextResponse.json({ error: "No books found" }, { status: 404 })
+        }
+
+        return NextResponse.json({ data: response }, { status: 200 })
+    }
+    catch (error) {
+        console.log(error)
+        return NextResponse.json( { error: "Unknown error", }, { status: 500 } )
+    }
+}
---
+++ types/searchRes.ts
@@ -0,0 +1,29 @@
+export interface SearchResponse {
+    numFound: number
+    start: number
+    numFoundExact: boolean
+    docs: Doc[]
+    num_found: number
+    q: string
+    offset: any
+  }
+  
+  export interface Doc {
+    key: string
+    title: string
+    author_name?: string[]
+    editions: Editions
+  }
+  
+  export interface Editions {
+    numFound: number
+    start: number
+    numFoundExact: boolean
+    docs: Doc2[]
+  }
+  
+  export interface Doc2 {
+    publish_date: string[]
+    isbn?: string[]
+  }
+  
