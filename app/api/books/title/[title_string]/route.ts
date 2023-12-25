26c029158384cf277c4d0c4129d6e9942de48ee9
S0lux <loctranphuoc456@protonmail.com>
2023-12-25 13:38:44 +0700

    Added check to prefer ISBN13 if possible

--- app/api/books/title/[title_string]/route.ts
+++ app/api/books/title/[title_string]/route.ts
@@ -16,7 +16,16 @@ export async function GET(request: NextRequest, { params }: { params: { title_st
 
         searches.docs.forEach(doc => {
             doc.editions.docs.forEach(book => {
-                if (book.isbn?.[0]) {
+
+                if (book.isbn?.[0] && book.isbn?.[1]) {
+                    response.push({
+                        Author: doc.author_name?.[0] || "Not found",
+                        Title: doc.title,
+                        ISBN13: book.isbn[0].length == 13 ? book.isbn[0] : book.isbn[1],
+                        PublishDate: (book.publish_date?.[0]) ? new Date(book.publish_date[0]).toISOString() : new Date().toISOString()
+                    })
+                }
+                else if (book.isbn?.[0]) {
                     response.push({
                         Author: doc.author_name?.[0] || "Not found",
                         Title: doc.title,
