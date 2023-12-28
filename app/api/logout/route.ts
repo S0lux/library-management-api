03c231bf318128150f670f5ef77fe1340ac3687a
S0lux <loctranphuoc456@protonmail.com>
2023-12-28 17:00:12 +0700

    Added return response to logout

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
@@ -10,5 +10,6 @@ export async function GET(request: NextRequest) {
     const jwtToken = request.headers.get("token")
     console.log("Logout request: " + jwtToken?.substring(0, 6))
 
-    redis.set(`bl_${jwtToken}`, jwtToken, { exat: Number(request.headers.get("exp")) })
+    await redis.set(`bl_${jwtToken}`, jwtToken, { exat: Number(request.headers.get("exp")) })
+    return NextResponse.json({ message: "Logged out" }, { status: 200 })
 }
