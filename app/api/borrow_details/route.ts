6bf73c4725e459c900f72d74554dd5bb8d1ef132
S0lux <loctranphuoc456@protonmail.com>
2023-12-28 11:21:30 +0700

    BorrowDetails api

---
+++ app/api/borrow_details/route.ts
@@ -0,0 +1,51 @@
+import { UpdateBookDetails } from "@/utils/bookDetailsUpdater";
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const Prisma = new PrismaClient();
+
+export async function PUT(request: NextRequest) {
+    
+    let body
+
+    try {
+        body = await request.json();
+    }
+    catch {
+        return NextResponse.json({ error: "Malformed request body"}, { status: 500 })
+    }
+    
+
+    if (!body.InvoiceID || !body.ISBN13 || !body.Returned || !body.Damaged || !body.Lost)
+    return NextResponse.json({ error: "Invalid request body"}, { status: 400 })
+
+    const updatedBorrowDetail = await Prisma.borrowDetail.update({
+        where: {
+            BorrowInvoiceID_ISBN13: {
+                BorrowInvoiceID: body.InvoiceID,
+                ISBN13: body.ISBN13,
+            }
+        },
+        data: {
+            Returned: body.Returned,
+            Damaged: body.Damaged,
+            Lost: body.Lost,
+        }
+    })
+
+    if (updatedBorrowDetail.Quantity == body.Returned + body.Lost + body.Damaged) {
+        const updatedBorrowDetail = await Prisma.borrowDetail.update({
+            where: {
+                BorrowInvoiceID_ISBN13: {
+                    BorrowInvoiceID: body.InvoiceID,
+                    ISBN13: body.ISBN13,
+                }
+            },
+            data: {
+                HasReturned: true
+            }
+        })
+    }
+
+    UpdateBookDetails(body.ISBN13, body.Returned, body.Quantity, body.Damaged, body.Lost)
+}
